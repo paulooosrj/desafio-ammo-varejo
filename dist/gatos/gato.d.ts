2e83eab8f62e6e7a608bbe372c0ded6410631598
Paulo Romão <jskhanframework@gmail.com>
2021-10-13 19:05:35 -0300

    Delete dist directory

--- dist/gatos/gato.d.ts
+++
@@ -1,7 +0,0 @@
-import { Document } from 'mongoose';
-export declare class Gato extends Document {
-    nome: string;
-    idade: number;
-    raca: string;
-}
-export declare const GatoSchema: import("mongoose").Schema<any>;
--- dist/produtos/produtos.d.ts
+++
@@ -1,7 +0,0 @@
-import { Document } from 'mongoose';
-export declare class Produtos extends Document {
-    nome: string;
-    imagem: string;
-    preco: string;
-}
-export declare const produtoschema: import("mongoose").Schema<any>;
